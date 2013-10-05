3849b0e6a281ffdba2513f98c8424a85ecb1674a
Jérémy Buget <jbuget@octo.com>
2013-10-06 00:39:19 +0200

    Improve BookmarkResource API (DELETE method return NO-CONTENT)

--- src/main/java/application/resources/BookmarkResourceImpl.java
+++ src/main/java/application/resources/BookmarkResourceImpl.java
@@ -7,12 +7,14 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.joda.time.DateTime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.google.common.collect.Lists;
 
@@ -28,24 +30,24 @@ public class BookmarkResourceImpl implements BookmarkResource {
     @Inject
     private BookmarkRepository bookmarkRepository;
 
+    @Override
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    @Override
     public Bookmark getBookmarkById(@PathVariable Long id) {
         Bookmark bookmark = bookmarkRepository.findOne(id);
         return bookmark;
     }
 
+    @Override
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    @Override
     public List<Bookmark> getAllBookmarks() {
         return Lists.newArrayList(bookmarkRepository.findAll());
     }
-
+    
+    @Override
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     @ResponseBody
-    @Override
     public Bookmark addBookmark(@RequestBody Bookmark bookmark) {
         if (bookmark.getId() == null) {
             bookmark.setCreationDate(new DateTime());
@@ -54,18 +56,18 @@ public class BookmarkResourceImpl implements BookmarkResource {
         return newBookmark;
     }
 
+    @Override
     @RequestMapping(method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
     @ResponseBody
-    @Override
     public Bookmark editBookmark(@RequestBody Bookmark bookmark) {
         bookmark.setModificationDate(new DateTime());
         Bookmark updatedBookmark = bookmarkRepository.save(bookmark);
         return updatedBookmark;
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
     @Override
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeBookmark(@PathVariable Long id) {
         bookmarkRepository.delete(id);
     }
--- src/main/java/infra/config/WebConfig.java
+++ src/main/java/infra/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -15,9 +16,16 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public InternalResourceViewResolver configureInternalResourceViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setPrefix("/WEB-INF/pages/");
         resolver.setSuffix(".jsp");
         return resolver;
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/public/**").addResourceLocations("/public/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
 }
