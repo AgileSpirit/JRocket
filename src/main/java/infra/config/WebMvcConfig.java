09a3b79f4c98a5f608b8309383f9ffc25a1aefb9
Jérémy Buget <jbuget@octo.com>
2013-10-23 18:50:52 +0200

    Add WebContentInterceptor in Spring MVC config

--- src/main/java/infra/config/WebMvcConfig.java
+++ src/main/java/infra/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package infra.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -12,6 +14,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("/static/app/");
+    }
+    
+    @Bean
+    public WebContentInterceptor webContentInterceptor() {
+        WebContentInterceptor interceptor = new WebContentInterceptor();
+        interceptor.setUseCacheControlHeader(true);
+        interceptor.setUseExpiresHeader(true);
+        interceptor.setCacheSeconds(31556926);
+        return interceptor;
     }
     
 }
