231f9e2f474c08de6a813e79c712a73f5bfb67b7
Jérémy Buget <jbuget@octo.com>
2014-02-10 23:39:45 +0100

    Add saving/updating feature with event emission/listening

--- src/main/webapp/static/app/scripts/controllers/BookmarkListController.js
+++ src/main/webapp/static/app/scripts/controllers/BookmarkListController.js
@@ -1,28 +1,34 @@
 'use strict';
 
 angular.module('jrocketApp')
-  .controller('BookmarkListController', ['$scope', 'BookmarkService',
-    function ($scope, bookmarkService) {
+  .controller('BookmarkListController', ['$scope', '$rootScope', 'BookmarkService',
+    function ($scope, $rootScope, bookmarkService) {
 
       // Instantiate an object to store your scope data in (Best Practices)
-      $scope.data = {};
-
-      $scope.data.bookmarks = bookmarkService.query();
+      $scope.bookmarks = {};
 
       $scope.removeBookmark = function (bookmark) {
         // Server call
         bookmarkService.remove({id: bookmark.id},
           function (value, responseHeaders) {
             // Client update
-            $scope.data.bookmarks = bookmarkService.query();
+            $scope.bookmarks = bookmarkService.query();
           },
           function (httpResponse) {
             console.log('An error occured during bookmark delete');
           });
       };
 
-//      $scope.editBookmark = function (id) {
-//        $rootScope.emit('wizard.open', id);
-//      };
+      $scope.editBookmark = function (id) {
+        $rootScope.$emit('openBookmarkWizardEvent', id);
+      };
+
+      $rootScope.$on('refreshBookmarkList', refreshBookmarkList());
+
+      function refreshBookmarkList() {
+        $scope.bookmarks = bookmarkService.query();
+      }
+
+      refreshBookmarkList();
 
     }]);
--- src/main/webapp/static/app/scripts/controllers/BookmarkWizardController.js
+++ src/main/webapp/static/app/scripts/controllers/BookmarkWizardController.js
@@ -3,48 +3,53 @@
 angular.module('jrocketApp')
   .controller('BookmarkWizardController', ['$scope', '$rootScope', 'BookmarkService', function ($scope, $rootScope, bookmarkService) {
 
-//    $rootScope.on('wizard.open', my_open_function);
-//    $rootScope.on('$destroy', do_unbind);
-
-    $scope.wizard = {};
-
     // Initialize the bookmark object that will be used for wizard (ADDING/EDITION)
-    $scope.wizard.bookmark = {};
+    $scope.bookmark = {};
 
     $scope.saveBookmark = function () {
       // Retrieve bookmark
-      var bookmark = $scope.wizard.bookmark;
+      var bookmark = $scope.bookmark;
       // Server call
       if (bookmark.id) {
         // Merge
         console.log('Updating bookmark...');
         bookmarkService.update({id: bookmark.id}, bookmark,
-          function () {
+          function() {
             console.log('Bookmark updated');
-          },
-          function () {
-            console.log('Bookmark update KO');
+            emitRefreshBookmarkListEvent();
           });
       } else {
         // Persist
         console.log('Saving bookmark...');
-        bookmarkService.save(bookmark);
-        console.log('Bookmark saved');
+        bookmarkService.save(bookmark,
+          function() {
+            console.log('Bookmark saved');
+            emitRefreshBookmarkListEvent();
+          });
       }
-      // Client update
-      this.closeBookmarkWizard();
+
+      // Close the wizard
+      closeBookmarkWizard();
     };
 
-    $scope.openBookmarkWizard = function (bookmarkId) {
-      $scope.wizard.bookmark = {};
+    function emitRefreshBookmarkListEvent() {
+      $rootScope.$emit('refreshBookmarkList');
+    }
+
+    $rootScope.$on('openBookmarkWizardEvent', function(event, args){
+      openBookmarkWizard(args);
+    });
+
+    function openBookmarkWizard(bookmarkId) {
+      $scope.bookmark = {};
       if (typeof bookmarkId != 'undefined') {
-        $scope.wizard.bookmark = bookmarkService.get({id: id});;
+        $scope.bookmark = bookmarkService.get({id: bookmarkId});
       }
       $('#bookmarkWizard').modal('show');
-    };
+    }
 
-    $scope.closeBookmarkWizard = function () {
+    function closeBookmarkWizard() {
       $('#bookmarkWizard').modal('hide');
-    };
+    }
 
   }]);
