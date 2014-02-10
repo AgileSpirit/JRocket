061be5a2a19652e4a8d25615fd0bfc25c2bdc29c
Jérémy Buget <jbuget@octo.com>
2014-02-10 09:16:23 +0100

    Rollback code reformatting

--- src/main/webapp/static/app/scripts/services/BookmarkService.js
+++ src/main/webapp/static/app/scripts/services/BookmarkService.js
@@ -4,7 +4,7 @@ var baseUrl = 'http://localhost:8080/JRocket';
 
 angular.module('jrocketApp')
   .service('BookmarkService', function ($resource) {
-    return $resource(baseUrl + '/api/bookmarks/:id', { id: '@id' }, {
-      update: {method: 'PUT', params: { id: '@id' } }
-    });
-  });
+        return $resource(baseUrl + '/api/bookmarks/:id', { id: '@id' }, {
+            update: {method: 'PUT', params: { id: '@id' } }
+          });
+      });
